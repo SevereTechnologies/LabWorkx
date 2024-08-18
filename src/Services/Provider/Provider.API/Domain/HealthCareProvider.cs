b9713d570011dd89dedec5def850aaf48c7f8100
Jon-Slem Severe <jonslem@gmail.com>
2024-08-18 09:16:15 -0700

    updated again

---
+++ src/Services/Provider/Provider.API/Domain/HealthCareProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace HealthProvider.API.Domain;
+
+public class HealthCareProvider
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Practice { get; set; } = default!;
+    public List<string> Specialties { get; set; } = new();
+    public string Manager { get; set; } = default!;
+    public string TaxId { get; set; } = default!;
+    public string NPI { get; set; } = default!;
+    public bool MedicalDoctor { get; set; } = default!;
+    public bool BoardCertified { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+}
---
+++ src/Services/Provider/Provider.API/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using ApplicationBlocks.CQRS;
+global using Carter;
+global using Mapster;
+global using Marten;
+global using MediatR;
